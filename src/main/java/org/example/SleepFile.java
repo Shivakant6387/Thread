4b0e58631d4b17063476bcc53e76ecdaeff765f9
Shivakant6387 <shivakant5875@gmail.com>
2022-12-19 12:25:33 +0530

    Added file sleep

--- src/main/java/org/example/SleepFile.java
+++ src/main/java/org/example/SleepFile.java
@@ -3,11 +3,23 @@ class numbers{
     public void generateNumbers(){
         for (int i=1;i<=100;i++){
             System.out.println("The numbers from 1 to 100 are "+i);
+        if (i==5){
+            try {
+                Thread.sleep(10000);
+            }catch (InterruptedException e){
+                System.out.println(e);
+            }
+        }
         }
     }
     public void generateNumbersFrom101(){
         for (int i=101;i<=200;i++){
             System.out.println("The numbers from 101 to 200 are "+i);
+            try {
+                Thread.sleep(10000);
+            }catch (InterruptedException e){
+                System.out.println(e);
+            }
         }
     }
 }
@@ -26,6 +38,8 @@ public class SleepFile{
                 thread.generateNumbersFrom101();
             }
         });
+        thread1.setPriority(10);
+        thread2.setPriority(1);
         thread1.start();
         thread2.start();
         System.out.println("Thanks for  using our software");
